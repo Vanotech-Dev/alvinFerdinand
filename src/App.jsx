edcc3353d950de18f720e3437844081f1827b210
Vanotech-Dev <vansstzy28@gmail.com>
2026-04-14 12:36:26 +0700

    fix: fixing hero section

--- src/App.jsx
+++ src/App.jsx
@@ -95,7 +95,7 @@ function App() {
       <main>
         {/* Hero Section */}
         <section
-          className="min-h-screen flex items-center pt-24 px-8 lg:pl-48 bg-white overflow-hidden"
+          className="min-h-screen flex items-center pt-28 px-8 lg:pl-32 bg-white overflow-hidden"
           id="home"
         >
           <div className="max-w-7xl mx-auto grid lg:grid-cols-[auto_1fr] gap-12 items-center w-full">
@@ -132,15 +132,8 @@ function App() {
                 <p className="text-primary-container text-lg sm:text-xl font-medium tracking-wide font-headline">
                   Motion Graphic Designer
                 </p>
-                <p className="text-neutral-500 text-base md:text-xl font-light leading-relaxed max-w-lg">
-                  Hi, I’m Alvin, a Motion Graphic Designer specializing in SaaS
-                  explainer videos and UI-driven motion design. I craft clean,
-                  modern visuals that simplify complex ideas through refined
-                  animation, minimal aesthetics, and purposeful
-                  storytelling—helping digital products communicate clearly,
-                  effectively, and with a premium feel. Focused on clarity,
-                  precision, and impact, I design motion that not only looks
-                  good, but works.
+                <p className="text-neutral-500 text-base md:text-xl font-light leading-relaxed max-w-2xl">
+                  Hi, I’m Alvin, a Motion Graphic Designer specializing in SaaS explainer videos and UI-driven motion design. I craft clean, modern visuals that simplify complex ideas through refined animation, minimal aesthetics, and purposeful storytelling—helping digital products communicate clearly, effectively, and with a premium feel. Focused on clarity, precision, and impact, I design motion that not only looks good, but works.
                 </p>
               </div>
               <div className="flex flex-wrap gap-4">
